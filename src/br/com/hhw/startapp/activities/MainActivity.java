881214698ec3f37a1aa0de39b5d8d403b863b382
henriqueho <henriqueho@gmail.com>
2015-03-12 16:47:20 -0300

    Implementando nova montagem do menu

--- src/br/com/hhw/startapp/activities/MainActivity.java
+++ src/br/com/hhw/startapp/activities/MainActivity.java
@@ -1,5 +1,9 @@
 package br.com.hhw.startapp.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -10,31 +14,56 @@ import android.widget.RelativeLayout;
 import br.com.hhw.startapp.R;
 import br.com.hhw.startapp.activities.helpers.CustomDialogHandler;
 import br.com.hhw.startapp.activities.helpers.CustomDialogHelpers;
+import br.com.hhw.startapp.activities.helpers.SharedPreferencesHelper;
+import br.com.hhw.startapp.fragments.HomeFragment;
+import br.com.hhw.startapp.fragments.TesteFragment;
+import br.com.how.DefaultFragment;
+import br.com.how.HHWMenuItem;
 import br.com.how.HHWSlideMenu;
 
 public class MainActivity extends ActionBarActivity {
 
 	private HHWSlideMenu slideMenu;
 
+	private void createMenu() {
+		
+		TesteFragment fragment = new TesteFragment();
+		
+		
+		ArrayList<HHWMenuItem> menuItems = new ArrayList<HHWMenuItem>();
+
+		menuItems.add(new HHWMenuItem("Início", new HomeFragment(), getResources().getDrawable(R.drawable.abc_btn_check_material)));
+		menuItems.add(new HHWMenuItem("Meus pontos", fragment.newInstance("Meus pontos")));
+		menuItems.add(new HHWMenuItem("Minhas rotas", fragment.newInstance("Minhas rotas")));
+		menuItems.add(new HHWMenuItem("Saiba mais", fragment.newInstance("Saiba mais")));
+		menuItems.add(new HHWMenuItem("Como usar?", TutorialActivity.class));
+		menuItems.add(new HHWMenuItem("Configurações", fragment.newInstance("Configurações")));
+		menuItems.add(new HHWMenuItem("Sobre", fragment.newInstance("Sobre")));
+		
+		slideMenu = new HHWSlideMenu(this, menuItems);
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_slide_menu);
-		
+
 		hackyToResolveBugToShowActionBar();
-        
-		slideMenu = new HHWSlideMenu(this);
+
+		createMenu();
 
 		// enable ActionBar app icon to behave as action to toggle nav drawer
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		getSupportActionBar().setHomeButtonEnabled(true);
-		
+
+		showTutorial();
+
 		// Abrindo home no slidemenu
 		if (savedInstanceState == null) {
 			slideMenu.openItemMenu(slideMenu.STARTPAGE);
 		}
 	}
-	
+
 	@Override
 	public void onBackPressed() {
 		slideMenu.mDrawerLayout.closeDrawers();
@@ -72,23 +101,32 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	private void hackyToResolveBugToShowActionBar() {
 		RelativeLayout hacky = (RelativeLayout) findViewById(R.id.drawer_content);
-        WindowManager.LayoutParams attrs = getWindow().getAttributes();
-        attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
-        getWindow().setAttributes(attrs);
-        hacky.setVisibility(View.GONE);
-        hacky.setVisibility(View.VISIBLE);
+		WindowManager.LayoutParams attrs = getWindow().getAttributes();
+		attrs.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+		getWindow().setAttributes(attrs);
+		hacky.setVisibility(View.GONE);
+		hacky.setVisibility(View.VISIBLE);
 	}
-	
+
+	private void showTutorial() {
+		if (SharedPreferencesHelper.getInstance(this).hasToShowTutorial()) {
+			SharedPreferencesHelper.getInstance(this).setHasToShowTutorial(
+					false);
+			startActivity(new Intent(this, TutorialActivity.class));
+		}
+	}
+
 	private void showDialogToFinish() {
 		String text = getString(R.string.voce_realmente_deseja_sair_do_aplicativo_);
-		CustomDialogHelpers.showGenericDialog(this, text, new CustomDialogHandler() {
-			@Override
-			public void setOk() {
-				finish();
-			}	
-		});
+		CustomDialogHelpers.showGenericDialog(this, text,
+				new CustomDialogHandler() {
+					@Override
+					public void setOk() {
+						finish();
+					}
+				});
 	}
 }
